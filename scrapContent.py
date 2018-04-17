4947e25b283638a9f0728e0fc3ca111a9d8b9c9f
Rayan Samy Ramoul <26028963+raysr@users.noreply.github.com>
2018-04-17 16:44:16 +0100

    add scraper of contents

---
+++ scrapContent.py
@@ -0,0 +1,55 @@
+from urllib.parse import urljoin
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from bs4.element import Comment
+import urllib.request
+from bs4 import BeautifulSoup
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+
+def scrap(body):
+    global web
+    web.get(str(body))
+    html=web.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+    texts = soup.findAll(text=True)
+    visible_texts = filter(tag_visible, texts)  
+    return u" ".join(t.strip() for t in visible_texts)
+
+
+options = Options()
+browsers={}
+firefox_capabilities = DesiredCapabilities.FIREFOX
+firefox_capabilities['marionette'] = True
+options.add_argument("--headless")
+web=webdriver.Firefox(firefox_options=options,capabilities=firefox_capabilities)
+
+
+
+
+
+
+categories = ["Arts", "Computers", "Health", "News", "Reference", "Science", "Society",
+                "Business", "Games", "Home", "Recreation", "Regional", "Shopping", "Sports"]
+
+for d in categories:
+	succ=0
+	prob=0
+	f=open('SitesName/'+str(d),'r').read()
+	lis=f.split('^#$')
+	f2=open('SitesContents/'+str(d),'w')
+	f3=open('SitesNotScrapped/'+str(d),'w')
+	for i in lis:
+		try:
+			f2.write(str(scrap(i)))
+			print('Success : '+str(i))
+		except:
+			f3.write(str(i))
+			print('Problem : '+str(i))
+
+	f.close()
+	f2.close()
+	f3.close()
+	print(' Success : '+str(succ))
+	print(' Failed : '+str(prob))
+	print(' Percentage : '+str((succ/(prob+succ))*100))	
